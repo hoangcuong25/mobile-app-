5f4a16e9789286c3bf67e614a94836b3ec24b0c8
hoang cuong <vancuong25112003@gmail.com>
2025-08-08 02:04:31 +0700

    auth

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,15 @@
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
 import "../global.css"
 
 export default function RootLayout() {
   return (
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </ClerkProvider>
   );
 }
